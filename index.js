5b88780dff33eb3c3a1296d9726a2d2b6ebdba59
Sergey Baryshnikov <sergey.baryshnikov89@gmail.com>
2023-01-16 17:24:41 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// function sum() {
+//   let summa = 0;
+// for (let index = 0; index < arguments.length; index++) {
+//   summa += arguments[index];
+// }
+//   return summa;
+// }
+
+// // console.log(sum(1,2,3,7))
+
---
+++ js/myArray.js
@@ -0,0 +1,77 @@
+/**
+ *
+ * @param {[number]} array
+ * @returns {boolean}
+ */
+function compareNextValue(array) {
+    if (array.length < 2) {
+      return false;
+    }
+    for (let index = 1; index < array.length; index++) {
+      if (array[index] === array[index - 1]) {
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  console.log(compareNextValue([0,0]));
+
+// function MyArrayPrototype() {
+//   this.push = function () {
+//     for (let index = 0; index < arguments.length; index++) {
+//       this[this.length++] = arguments[index];
+//       //   ++this.length;
+//     }
+//     return this.length;
+//   };
+//   this.pop = function () {
+//     if (this.length === 0) {
+//       return;
+//     }
+//     const lastItem = this[this.length - 1];
+//     delete this[--this.length];
+//     // this.length--;
+//     return lastItem;
+//   };
+// }
+
+// function MyArray() {
+//   this.length = 0;
+//   for (let index = 0; index < arguments.length; index++) {
+//     this.push(arguments[index]);
+//   }
+// }
+
+// MyArray.prototype = new MyArrayPrototype();
+
+// const myArrayNumbers = new MyArray();
+// myArrayNumbers.push(777);
+// console.log(myArrayNumbers);
+
+// const arrayNumbers = [];
+// arrayNumbers.push(555);
+// console.log(arrayNumbers);
+
+// function isStringInArr(arr, string) {
+//   return arr.includes(string);
+// }
+
+// console.log(isStringInArr([], "qw"));
+
+// /**
+//  *
+//  * @param {[number]} arr
+//  * @returns {number | null}
+//  */
+// function getAverage(array) {
+//   if (array.length === 0) {
+//     return null;
+//   }
+//   let summa = 0;
+//   for (let index = 0; index < array.length; index++) {
+//     summa += array[index];
+//   }
+//   return summa / array.length;
+// }
+// console.log(getAverage([4, 5, 6]));
